05ef01d724695d43e86f3d959fd01eb8c5c88a8f
Randy Taylor <tehgecKozzz@gmail.com>
2018-04-10 12:07:08 -0400

    Begin kill process impl

--- pwatch/pwatch.c
+++ pwatch/pwatch.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <ctype.h>
+#include <dirent.h>
+#include <string.h>
 
 #include <sys/types.h>
 #include <sys/stat.h>
@@ -10,20 +12,61 @@
 
 // more: https://jakash3.wordpress.com/2011/06/17/linux-kill-all-processes-with-name/
 
-int main(int argc, char **argv) {
-    int pid;
-    sscanf(argv[1], "%d", &pid);
-    printf("pid: %d\n", pid);
+int kill_process(const char* name) {
+    DIR* dir = opendir("/proc");
 
-    char filename[1000];
-    sprintf(filename, "/proc/%d/cmdline", pid);
-    FILE *f = fopen(filename, "r");
+    if (dir == NULL) {
+        printf("Failed to open /proc directory\n");
+        return -1;
+    }
+    struct dirent *ent;
 
-    char test[1000];
-    fscanf(f, "%s", test);
-    printf("contents: %s", test);
-    fclose(f);
+    unsigned short skip_process = 0;
+
+    // Iterate through each folder in the directory.
+    while ((ent = readdir(dir)) != NULL) {
+        // Skip folders that aren't processes (if they contain characters
+        // that aren't numbers).
+        for (unsigned int i =0; i < strlen(ent->d_name); i++) {
+            if (!isdigit(ent->d_name[i])) {
+                skip_process = 1;
+                break;
+            }
+        }
+        if (skip_process) {
+            skip_process = 0;
+            continue;
+        }
+
+        printf("%s\n", ent->d_name);
+    }
+    closedir(dir);
+    
+    return 0;
 }
+
+int main(int argc, char** argv) {
+    const char* process_name = "test";
+    int success = kill_process(process_name);
+    return success;
+}
+
+// int main(int argc, char **argv) {
+//     int pid;
+//     sscanf(argv[1], "%d", &pid);
+//     printf("pid: %d\n", pid);
+// 
+//     char filename[1000];
+//     sprintf(filename, "/proc/%d/cmdline", pid);
+//     FILE *f = fopen(filename, "r");
+// 
+//     printf("%s", filename);
+// 
+//     char test[1000];
+//     fscanf(f, "%s", test);
+//     printf("contents: %s", test);
+//     fclose(f);
+// }
 
 /*
 int main() {
