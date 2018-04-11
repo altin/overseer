89e908b01e0785f450c8c8e29a2da8ba2f3ad263
Randy Taylor <tehgecKozzz@gmail.com>
2018-04-11 00:42:44 -0400

    Remove overseen-uh.sh execution from profile on uninstall

--- inject.c
+++ inject.c
@@ -9,31 +9,42 @@ const char run_script[] = "sh ./overseen-ug.sh";
 FILE* profile;
 
 void install() {
-    system(add_group); // Create group
+    system(add_group);
     system(cp_script); // Copy shell script to /etc
-    profile = fopen("/etc/profile", "a"); // Open /etc/profile in append mode
+    profile = fopen("/etc/profile", "a");
     fprintf(profile, "%s", run_script); // Append shell execution command to /etc/profile
     fclose(profile);
 }
 
 void uninstall() {
-    profile = fopen("/etc/profile", "r+"); // Open /etc/profile in read/write mode
-    char buffer [256];
+    // TODO(randy/altin): Error handling.
+    profile = fopen("/etc/profile", "r");
+    FILE* temp = fopen("/etc/profile_temp", "w");
+    char buffer[256];
+
     while (fgets(buffer, sizeof(buffer), profile)) {
-        if (strcmp(buffer, run_script) != 0) {
-            fputs(buffer, profile);
+        if (strcmp(buffer, run_script) == 0) {
+            continue;
+        } else if (strcmp(buffer, "\n") == 0) {
+            strcat(buffer, "");
         }
+        fputs(buffer, temp);
         printf("Add: %s", buffer);
     }
+
     fclose(profile);
+    fclose(temp);
+
+    remove("/etc/profile");
+    rename("/etc/profile_temp", "/etc/profile");
 }
 
 int main(int argc, char** argv) {
     if (strcmp(argv[1], "install") == 0) {
         install();
-    }
-    else if (strcmp(argv[1], "uninstall") == 0) {
+    } else if (strcmp(argv[1], "uninstall") == 0) {
         uninstall();
     }
+
     return 0;
 }
