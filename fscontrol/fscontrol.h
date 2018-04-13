a161112a97717f38045bba5c7eb993db02ad016b
Randy Taylor <tehgecKozzz@gmail.com>
2018-04-12 23:24:16 -0400

    Fix compilation issues and update gitignore

--- fscontrol/fscontrol.h
+++ fscontrol/fscontrol.h
@@ -1,6 +1,14 @@
-#ifndef FSCONTROL_H
-#define FSCONTROL_H
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
 
-void fperms (char*, char*);
-
-#endif
+void fperms (char* path, char* mode) {
+    long perms = strtol(mode, 0, 8); // Convert permission string to long
+    if(chmod(path, perms) < 0) { // Chmod system call to update file permissions
+        printf("Error: %s\n", strerror(errno));
+    } else {
+        printf("Updated permissions of %s to %s (%ld)\n", path, mode, perms);
+    }
+}
