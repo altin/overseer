ebba03ffd18fa4ce6caeeb763c10da094804e348
Randy Taylor <tehgecKozzz@gmail.com>
2018-04-13 19:45:54 -0400

    Fix autostart dir not being created

--- src/scripts/overseen_ug.sh
+++ src/scripts/overseen_ug.sh
@@ -4,6 +4,7 @@ DHCLIENT=$(head -n 1 /etc/overseer/src/scripts/overseer_dhpath)
 DHPATH=$(dirname $DHCLIENT)
 DHCLIENTOLD="$DHPATH/overseer_dhclient_sudo.conf"
 OPENDNS="supersede domain-name-servers 208.67.222.123,208.67.220.123;"
+USERNAME=$(whoami)
 
 if [ $GROUP != "overseen" ]; then
     if grep -q "$OPENDNS" $DHCLIENT; then
@@ -14,6 +15,6 @@ fi
 
 if [ $GROUP = "overseen" ]; then
     echo "Overseen user logged in"
-    mkdir $HOME/.config/autostart
+    mkdir -p /home/$USERNAME/.config/autostart
     cp /etc/overseer/src/scripts/pcontrol.desktop $HOME/.config/autostart/
 fi
